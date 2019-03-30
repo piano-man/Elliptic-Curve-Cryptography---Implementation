5c3063756698d9bff4a2fe36e97ba1acdfb69803
piano-man <icm2015003@iiita.ac.in>
2019-03-30 15:03:49 +0530

    prime order exhaustive search added

--- ecc.py
+++ ecc.py
@@ -6,8 +6,15 @@ Point = namedtuple("Point", "x y")
 
 O = 'Origin'
 
-a = 2
-b = 2
+a = 1
+b = 3
+
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
 
 def valid(P):
     # print((P.y**2)%p)
@@ -69,24 +76,23 @@ def plot_curve(p,a,b):
     plt.show()
 
 def curve_under_field(p,a,b):
-    for i in range(1000):
-        if i in range(0,p):
-            j = i**3 + a*i + b
-            j_final = sqrt_mod(j,p,True)
-            # print(j_final)
-            if j_final==None:
-                continue
-            else:
-                if type(j_final)==list and len(j_final) > 1:
-                    # print(j_final[0])
-                    # print(j_final[1])
-                    x_field.append(i)
-                    x_field.append(i)
-                    # print("Modulus :-",j%p,(j_final[0]**2)%p,(j_final[1]**2)%p)
-                    y_field.append(j_final[0])
-                    y_field.append(j_final[1])
-            # plt.scatter(i,j)
-    # plt.show()
+    for i in range(p):
+        j = i**3 + a*i + b
+        j_final = sqrt_mod(j,p,True)
+        # print(j_final)
+        if j_final==None:
+            continue
+        else:
+            if type(j_final)==list and len(j_final) > 1:
+                # print(j_final[0])
+                # print(j_final[1])
+                x_field.append(i)
+                x_field.append(i)
+                # print("Modulus :-",j%p,(j_final[0]**2)%p,(j_final[1]**2)%p)
+                y_field.append(j_final[0])
+                y_field.append(j_final[1])
+        # plt.scatter(i,j)
+# plt.show()
 
 def find_order(P):
     order = 0
@@ -100,15 +106,24 @@ def find_order(P):
         print(temp)
     return order+2
 
-p = 17
+p = 15733
 plot_curve(p,a,b)
 x_field = []
 y_field = []
 curve_under_field(p,a,b)
 
 # P = Point(x_field[2],y_field[2])
-P = Point(x=5,y=1)
-print(P)
-print(valid(P))
-print(find_order(P))
-# print(ec_inv(P))
+
+#searching for P with prime order q
+prime_order = 0
+prime_order_point = Point(0,0)
+l = len(x_field)
+for i in range(l):
+    P = Point(x_field[i],y_field[i])
+    order = find_order(P)
+    if isPrime(order):
+        prime_order = order
+        prime_order_point = P
+        break
+
+print(prime_order,prime_order_point)
